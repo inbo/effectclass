c314c7ec1659cf57a2d8afa9acb6716a72b8d94f
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2020-01-04 14:22:56 +0100

    more unit tests

--- tests/testthat/test_aa_classification.R
+++ tests/testthat/test_aa_classification.R
@@ -6,9 +6,16 @@ test_that("classification() works with a single threshold", {
   expect_is(remove_sign(z), "effectclass")
   expect_is(remove_sign(remove_sign(z)), "effectclass")
   expect_identical(as.character(remove_sign(z)), "?")
+  expect_identical(
+    as.character(coarse_classification(remove_sign(z))),
+    "?"
+  )
   z <- coarse_classification(z)
   expect_is(z, "effectclass")
   expect_identical(as.character(z), "?")
+  expect_is(remove_sign(z), "effectclass")
+  expect_is(remove_sign(remove_sign(z)), "effectclass")
+  expect_identical(as.character(remove_sign(z)), "?")
   z <- coarse_classification(z)
   expect_is(z, "effectclass")
   expect_identical(as.character(z), "?")
@@ -16,56 +23,101 @@ test_that("classification() works with a single threshold", {
   z <- classification(lcl = 0.5, ucl = 2, threshold = 1)
   expect_identical(as.character(z), "+")
   expect_identical(as.character(remove_sign(z)), "*")
+  expect_identical(
+    as.character(coarse_classification(remove_sign(z))),
+    "*"
+  )
   z <- coarse_classification(z)
   expect_identical(as.character(z), "+")
+  expect_identical(as.character(remove_sign(z)), "*")
 
   z <- classification(lcl = 1.5, ucl = 2, threshold = 1)
   expect_identical(as.character(z), "++")
   expect_identical(as.character(remove_sign(z)), "**")
+  expect_identical(
+    as.character(coarse_classification(remove_sign(z))),
+    "*"
+  )
   z <- coarse_classification(z)
   expect_identical(as.character(z), "+")
+  expect_identical(as.character(remove_sign(z)), "*")
 
   z <- classification(lcl = 0.5, ucl = 0.9, threshold = 1)
   expect_identical(as.character(z), "+~")
   expect_identical(as.character(remove_sign(z)), "*~")
+  expect_identical(
+    as.character(coarse_classification(remove_sign(z))),
+    "*"
+  )
   z <- coarse_classification(z)
   expect_identical(as.character(z), "+")
+  expect_identical(as.character(remove_sign(z)), "*")
 
   z <- classification(lcl = -0.9, ucl = -0.5, threshold = 1)
   expect_identical(as.character(z), "-~")
   expect_identical(as.character(remove_sign(z)), "*~")
+  expect_identical(
+    as.character(coarse_classification(remove_sign(z))),
+    "*"
+  )
   z <- coarse_classification(z)
   expect_identical(as.character(z), "-")
+  expect_identical(as.character(remove_sign(z)), "*")
 
   z <- classification(lcl = -1.5, ucl = -0.5, threshold = 1)
   expect_identical(as.character(z), "-")
   expect_identical(as.character(remove_sign(z)), "*")
+  expect_identical(
+    as.character(coarse_classification(remove_sign(z))),
+    "*"
+  )
   z <- coarse_classification(z)
   expect_identical(as.character(z), "-")
+  expect_identical(as.character(remove_sign(z)), "*")
 
   z <- classification(lcl = -2, ucl = -1.5, threshold = 1)
   expect_identical(as.character(z), "--")
   expect_identical(as.character(remove_sign(z)), "**")
+  expect_identical(
+    as.character(coarse_classification(remove_sign(z))),
+    "*"
+  )
   z <- coarse_classification(z)
   expect_identical(as.character(z), "-")
+  expect_identical(as.character(remove_sign(z)), "*")
 
   z <- classification(lcl = -0.5, ucl = 0.5, threshold = 1)
   expect_identical(as.character(z), "~")
   expect_identical(as.character(remove_sign(z)), "~")
+  expect_identical(
+    as.character(coarse_classification(remove_sign(z))),
+    "~"
+  )
   z <- coarse_classification(z)
   expect_identical(as.character(z), "~")
+  expect_identical(as.character(remove_sign(z)), "~")
 
   z <- classification(lcl = -1.5, ucl = 0.5, threshold = 1)
   expect_identical(as.character(z), "?-")
   expect_identical(as.character(remove_sign(z)), "?*")
+  expect_identical(
+    as.character(coarse_classification(remove_sign(z))),
+    "?"
+  )
   z <- coarse_classification(z)
   expect_identical(as.character(z), "?")
+  expect_identical(as.character(remove_sign(z)), "?")
 
   z <- classification(lcl = -0.5, ucl = 1.5, threshold = 1)
   expect_identical(as.character(z), "?+")
   expect_identical(as.character(remove_sign(z)), "?*")
+  expect_identical(
+    as.character(coarse_classification(remove_sign(z))),
+    "?"
+  )
   z <- coarse_classification(z)
   expect_identical(as.character(z), "?")
+  expect_identical(as.character(remove_sign(z)), "?")
 })
 test_that("classification() works with two thresholds", {
   expect_identical(
