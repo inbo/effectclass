36fb71a62b098bf69dc904a846860684dc9bf273
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2020-01-04 14:04:36 +0100

    add more unit tests

--- tests/testthat/test_aa_classification.R
+++ tests/testthat/test_aa_classification.R
@@ -1,46 +1,71 @@
 context("classification")
 test_that("classification() works with a single threshold", {
-  expect_is(
-    z <- classification(lcl = -2, ucl = 2, threshold = 1),
-    "effectclass"
-  )
+  z <- classification(lcl = -2, ucl = 2, threshold = 1)
+  expect_is(z,"effectclass")
   expect_identical(as.character(z), "?")
-  expect_identical(
-    as.character(classification(lcl = 0.5, ucl = 2, threshold = 1)),
-    "+"
-  )
-  expect_identical(
-    as.character(classification(lcl = 1.5, ucl = 2, threshold = 1)),
-    "++"
-  )
-  expect_identical(
-    as.character(classification(lcl = 0.5, ucl = 0.9, threshold = 1)),
-    "+~"
-  )
-  expect_identical(
-    as.character(classification(lcl = -0.9, ucl = -0.5, threshold = 1)),
-    "-~"
-  )
-  expect_identical(
-    as.character(classification(lcl = -1.5, ucl = -0.5, threshold = 1)),
-    "-"
-  )
-  expect_identical(
-    as.character(classification(lcl = -2, ucl = -1.5, threshold = 1)),
-    "--"
-  )
-  expect_identical(
-    as.character(classification(lcl = -0.5, ucl = 0.5, threshold = 1)),
-    "~"
-  )
-  expect_identical(
-    as.character(classification(lcl = -1.5, ucl = 0.5, threshold = 1)),
-    "?-"
-  )
-  expect_identical(
-    as.character(classification(lcl = -0.5, ucl = 1.5, threshold = 1)),
-    "?+"
-  )
+  expect_is(remove_sign(z), "effectclass")
+  expect_is(remove_sign(remove_sign(z)), "effectclass")
+  expect_identical(as.character(remove_sign(z)), "?")
+  z <- coarse_classification(z)
+  expect_is(z, "effectclass")
+  expect_identical(as.character(z), "?")
+  z <- coarse_classification(z)
+  expect_is(z, "effectclass")
+  expect_identical(as.character(z), "?")
+
+  z <- classification(lcl = 0.5, ucl = 2, threshold = 1)
+  expect_identical(as.character(z), "+")
+  expect_identical(as.character(remove_sign(z)), "*")
+  z <- coarse_classification(z)
+  expect_identical(as.character(z), "+")
+
+  z <- classification(lcl = 1.5, ucl = 2, threshold = 1)
+  expect_identical(as.character(z), "++")
+  expect_identical(as.character(remove_sign(z)), "**")
+  z <- coarse_classification(z)
+  expect_identical(as.character(z), "+")
+
+  z <- classification(lcl = 0.5, ucl = 0.9, threshold = 1)
+  expect_identical(as.character(z), "+~")
+  expect_identical(as.character(remove_sign(z)), "*~")
+  z <- coarse_classification(z)
+  expect_identical(as.character(z), "+")
+
+  z <- classification(lcl = -0.9, ucl = -0.5, threshold = 1)
+  expect_identical(as.character(z), "-~")
+  expect_identical(as.character(remove_sign(z)), "*~")
+  z <- coarse_classification(z)
+  expect_identical(as.character(z), "-")
+
+  z <- classification(lcl = -1.5, ucl = -0.5, threshold = 1)
+  expect_identical(as.character(z), "-")
+  expect_identical(as.character(remove_sign(z)), "*")
+  z <- coarse_classification(z)
+  expect_identical(as.character(z), "-")
+
+  z <- classification(lcl = -2, ucl = -1.5, threshold = 1)
+  expect_identical(as.character(z), "--")
+  expect_identical(as.character(remove_sign(z)), "**")
+  z <- coarse_classification(z)
+  expect_identical(as.character(z), "-")
+
+  z <- classification(lcl = -0.5, ucl = 0.5, threshold = 1)
+  expect_identical(as.character(z), "~")
+  expect_identical(as.character(remove_sign(z)), "~")
+  z <- coarse_classification(z)
+  expect_identical(as.character(z), "~")
+
+  z <- classification(lcl = -1.5, ucl = 0.5, threshold = 1)
+  expect_identical(as.character(z), "?-")
+  expect_identical(as.character(remove_sign(z)), "?*")
+  z <- coarse_classification(z)
+  expect_identical(as.character(z), "?")
+
+  z <- classification(lcl = -0.5, ucl = 1.5, threshold = 1)
+  expect_identical(as.character(z), "?+")
+  expect_identical(as.character(remove_sign(z)), "?*")
+  z <- coarse_classification(z)
+  expect_identical(as.character(z), "?")
 })
 test_that("classification() works with two thresholds", {
   expect_identical(
