6b28640b03afc3b236c41f07eb20d8ff84afed9b
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2020-01-10 14:25:16 +0100

    replace plot symbols for "+", "-" and "*" with the detailed classification

--- data-raw/palette.R
+++ data-raw/palette.R
@@ -1,9 +1,9 @@
 detailed_signed_palette <- c(
-  "\u25b2", "\u25e5", "\u25d3", "\u25c9", "\u25d2", "\u25e2", "\u25bc",
+  "\u25b2", "\u25b8", "\u25d3", "\u25c9", "\u25d2", "\u25c2", "\u25bc",
   "\u25b3", "\u25bd", "\u25c7"
 )
 detailed_unsigned_palette <- c(
-  "\u25b6", "\u25e5", "\u25d3", "\u25c9", "\u25b7", "\u25c7"
+  "\u25b6", "\u25b2", "\u25d3", "\u25c9", "\u25b7", "\u25c7"
 )
 coarse_signed_palette <- c("\u25b2", "\u25c9", "\u25bc", "\u25c7")
 coarse_unsigned_palette <- c("\u25b6", "\u25c9", "\u25c7")
