f41c30eef85bedbcd3516c164b5a564f33a426f1
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2022-04-21 11:31:36 +0200

    use rgb values instead of named colours

--- data-raw/palette.R
+++ data-raw/palette.R
@@ -1,16 +1,16 @@
 detailed_signed_palette <- c(
   `++` = "#65E060", `+` = "#A1B600", `+~` = "#B68B00", `~` = "#356196",
-  `-~` = "#9C4413", `-` = "#842502", `--` = "#6D0000", `?+` = "grey65",
-  `?-` = "grey35", `?` = "grey50"
+  `-~` = "#9C4413", `-` = "#842502", `--` = "#6D0000", `?+` = "A6A6A6",
+  `?-` = "5A5A5A", `?` = "#808080"
 )
 detailed_unsigned_palette <- c(
   `**` = "#65E060", `*` = "#A1B600", `*~` = "#B68B00", `~` = "#356196",
-  `?*` = "grey65", `?` = "grey50"
+  `?*` = "A6A6A6", `?` = "#808080"
 )
 coarse_signed_palette <- c(
-  `+` = "#A1B600", `~` = "#356196", `-` = "#842502", `?` = "grey50"
+  `+` = "#A1B600", `~` = "#356196", `-` = "#842502", `?` = "#808080"
 )
-coarse_unsigned_palette <- c(`*` = "#A1B600", `~` = "#356196", `?` = "grey50")
+coarse_unsigned_palette <- c(`*` = "#A1B600", `~` = "#356196", `?` = "#808080")
 usethis::use_data(
   detailed_signed_palette, detailed_unsigned_palette,
   coarse_signed_palette, coarse_unsigned_palette,
