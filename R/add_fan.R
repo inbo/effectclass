3f1bec29f7a19965bfea22f1dfafc19941962f8d
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2023-08-31 14:15:50 +0200

    add_fan() uses add_ribbons()

--- R/add_fan.R
+++ R/add_fan.R
@@ -33,14 +33,15 @@
 #'   add_fan(sd = ~s, max_prob = 0.95, link = "log")
 #' @family plotly add-ons
 #' @export
-#' @importFrom assertthat assert_that is.flag is.number noNA
-#' @importFrom plotly add_polygons
+#' @importFrom assertthat assert_that is.flag is.number is.string noNA
+#' @importFrom plotly add_ribbons
 add_fan <- function(
   p, x = NULL, y = NULL, ..., sd, link = c("identity", "log", "logit"),
-  max_prob = 0.9, step = 0.05, fillcolor = "blue", data = NULL, inherit = TRUE
+  max_prob = 0.9, step = 0.05, fillcolor = coarse_unsigned_palette[2],
+  data = NULL, inherit = TRUE
 ) {
   assert_that(
-    is.flag(inherit), noNA(inherit),
+    is.flag(inherit), noNA(inherit), is.string(fillcolor), noNA(fillcolor),
     is.number(max_prob), 0 < max_prob, max_prob < 1,
     is.number(step), noNA(step), 0 < step, step <= max_prob
   )
@@ -55,9 +56,9 @@ add_fan <- function(
   )
   for (prob in seq(max_prob, 1e-6, by = -step)) {
     p |>
-      add_polygons(
-        x = x, y = y, showlegend = FALSE, opacity = step, inherit = FALSE,
-        line = list(width = 0), fillcolor = fillcolor, ...,
+      add_ribbons(
+        x = ~x, ymin = ~lcl, ymax = ~ucl, showlegend = FALSE, opacity = step,
+        inherit = FALSE, line = list(width = 0), fillcolor = fillcolor, ...,
         data = error_ribbon(
           data = data, x = x, y = y, sd = sd, prob = prob, link = link
         )
@@ -88,17 +89,17 @@ error_ribbon <- function(
     has_name(data, as.character(c(x[[2]], y[[2]], sd[[2]]))),
     is.number(prob), 0 < prob, prob < 1
   )
+  stopifnot(
+    "`y` is not numeric" = is.numeric(data[[y[[2]]]]),
+    "`sd` is not numeric" = is.numeric(data[[sd[[2]]]])
+  )
   link <- match.arg(link)
-  x_1 <- data[[x[[2]]]]
   y_1 <- data[[y[[2]]]]
   sd_1 <- data[[sd[[2]]]]
   y_1 <- switch(link, identity = y_1, log = log(y_1), logit = qlogis(y_1))
-  x_2 <- c(x_1, rev(x_1))
-  y_2 <- c(
-    qnorm(0.5 + prob / 2, mean = y_1, sd = sd_1),
-    qnorm(0.5 - prob / 2, mean = rev(y_1), sd = rev(sd_1))
-  )
-  y_2 <- switch(link, identity = y_2, log = exp(y_2), logit = plogis(y_2))
-  data.frame(x = x_2, y = y_2, prob = prob) |>
-    `colnames<-`(as.character(c(x[[2]], y[[2]], "prob")))
+  lcl <- qnorm(0.5 - prob / 2, mean = y_1, sd = sd_1)
+  ucl <- qnorm(0.5 + prob / 2, mean = y_1, sd = sd_1)
+  lcl <- switch(link, identity = lcl, log = exp(lcl), logit = plogis(lcl))
+  ucl <- switch(link, identity = ucl, log = exp(ucl), logit = plogis(ucl))
+  data.frame(x = data[[x[[2]]]], lcl = lcl, ucl = ucl, prob = prob)
 }
