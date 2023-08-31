4431e12295c79e5fd710f18b9c893a47555144e0
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2023-08-31 22:00:37 +0200

    reference_shape() gains a line argument

--- R/reference_shape.R
+++ R/reference_shape.R
@@ -4,6 +4,8 @@
 #' @inheritParams classification
 #' @param colour The colour for the references.
 #' Defaults to `"black"`.
+#' @param line display the `threshold` as a line (`TRUE`) or a ribbon (`FALSE`).
+#' Defaults to `FALSE`.
 #' @param horizontal Display horizontal reference when `TRUE` (default).
 #'  Display vertical reference when `FALSE`.
 #' @template example_effect_data
@@ -12,12 +14,12 @@
 #' @export
 #' @importFrom assertthat assert_that is.flag is.number is.string noNA
 reference_shape <- function(
-  threshold, reference = 0, colour = "black", horizontal = TRUE
+  threshold, reference = 0, colour = "black", line = FALSE, horizontal = TRUE
 ) {
   assert_that(
     is.numeric(threshold), noNA(threshold), is.number(reference),
     noNA(reference), is.string(colour), noNA(colour), is.flag(horizontal),
-    noNA(horizontal)
+    noNA(horizontal), is.flag(line), noNA(line)
   )
   if (length(threshold) == 1) {
     threshold <- reference + c(-1, 1) * abs(threshold)
@@ -34,32 +36,48 @@ reference_shape <- function(
       y1 = reference, line = list(color = colour, dash = "longdash"),
       layer = "below"
     )
-    threshold_line_up <- list(
-      type = "line", x0 = 0, x1 = 1, xref = "paper", y0 = threshold[1],
-      y1 = threshold[1], line = list(color = colour, dash = "dot"),
-      layer = "below"
+    if (line) {
+      threshold_line_up <- list(
+        type = "line", x0 = 0, x1 = 1, xref = "paper", y0 = threshold[1],
+        y1 = threshold[1], line = list(color = colour, dash = "dot"),
+        layer = "below"
+      )
+      threshold_line_down <- list(
+        type = "line", x0 = 0, x1 = 1, xref = "paper", y0 = threshold[2],
+        y1 = threshold[2], line = list(color = colour, dash = "dot"),
+        layer = "below"
+      )
+      return(list(ref_line, threshold_line_up, threshold_line_down))
+    }
+    threshold_rect <- list(
+      type = "rect", x0 = 0, x1 = 1, xref = "paper", y0 = threshold[1],
+      y1 = threshold[2], line = list(width = 0), opacity = 0.1,
+      fillcolor = colour, layer = "below"
     )
-    threshold_line_down <- list(
-      type = "line", x0 = 0, x1 = 1, xref = "paper", y0 = threshold[2],
-      y1 = threshold[2], line = list(color = colour, dash = "dot"),
-      layer = "below"
-    )
-    return(list(ref_line, threshold_line_up, threshold_line_down))
+    return(list(threshold_rect, ref_line))
   }
   ref_line <- list(
     type = "line", y0 = 0, y1 = 1, yref = "paper", x0 = reference,
     x1 = reference, line = list(color = colour, dash = "longdash"),
     layer = "below"
   )
-  threshold_line_up <- list(
-    type = "line", y0 = 0, y1 = 1, yref = "paper", x0 = threshold[1],
-    x1 = threshold[1], line = list(color = colour, dash = "dot"),
-    layer = "below"
+  if (line) {
+    threshold_line_up <- list(
+      type = "line", y0 = 0, y1 = 1, yref = "paper", x0 = threshold[1],
+      x1 = threshold[1], line = list(color = colour, dash = "dot"),
+      layer = "below"
+    )
+    threshold_line_down <- list(
+      type = "line", y0 = 0, y1 = 1, yref = "paper", x0 = threshold[2],
+      x1 = threshold[2], line = list(color = colour, dash = "dot"),
+      layer = "below"
+    )
+    return(list(ref_line, threshold_line_up, threshold_line_down))
+  }
+  threshold_rect <- list(
+    type = "rect", y0 = 0, y1 = 1, yref = "paper", x0 = threshold[1],
+    x1 = threshold[2], line = list(width = 0), opacity = 0.1,
+    fillcolor = colour, layer = "below"
   )
-  threshold_line_down <- list(
-    type = "line", y0 = 0, y1 = 1, yref = "paper", x0 = threshold[2],
-    x1 = threshold[2], line = list(color = colour, dash = "dot"),
-    layer = "below"
-  )
-  list(ref_line, threshold_line_up, threshold_line_down)
+  return(list(threshold_rect, ref_line))
 }
--- man-roxygen/example_effect_plotly.R
+++ man-roxygen/example_effect_plotly.R
@@ -9,7 +9,7 @@
 #'   add_classification(
 #'     lcl = ~lcl, ucl = ~ucl, threshold = 1, detailed = FALSE
 #'    ) |>
-#'   layout(shapes = reference_shape(threshold = 1))
+#'   layout(shapes = reference_shape(threshold = 1, line = TRUE))
 #' plot_ly(z, x = ~x, y = ~estimate) |>
 #'   add_fan(sd = ~sd, step = 0.2) |>
 #'   add_classification(
@@ -21,7 +21,7 @@
 #'   add_classification(
 #'     lcl = ~lcl, ucl = ~ucl, threshold = 1, detailed = FALSE, signed = FALSE
 #'   ) |>
-#'   layout(shapes = reference_shape(threshold = 1))
+#'   layout(shapes = reference_shape(threshold = 1, line = TRUE))
 #'
 #' # trend
 #' plot_ly(data = trend, x = ~year, y = ~index) |>
